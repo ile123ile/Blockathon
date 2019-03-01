39e11f438bec687f67881354c830823f1d1f3505
Justin Chen <justinchen@dyn-160-39-130-39.dyn.columbia.edu>
2019-03-01 16:01:46 -0500

    added light controller and light states

---
+++ Blockathon/Assets/Scripts/StatesController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatesController : MonoBehaviour
+{
+    Light lt;
+    public int colorid;
+    State[] states = { new State(0, Color.red), new State(1, Color.green) };
+    LightState[] lightstates;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+
+        lightstates = (LightState[]) FindObjectsOfType(typeof(LightState));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lt.color = states[colorid].color;
+
+        foreach (LightState e in lightstates)
+        {
+            Renderer rend = e.gameObject.GetComponent<Renderer>();
+            if (e.colorid == this.colorid)
+            {
+                rend.enabled = true;
+            }
+            else
+            {
+                rend.enabled = false;
+            }
+        }
+    }
+
+    struct State
+    {
+        public int id;
+        public Color color;
+
+        public State(int id, Color color)
+        {
+            this.id = id;
+            this.color = color;
+        }
+    }
+
+}
