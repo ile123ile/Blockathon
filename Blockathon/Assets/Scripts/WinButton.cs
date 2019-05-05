dac79d438ad67a5c7f5c5dfae5e1efc1bfa3b913
ile123ile <bbravencofounder@gmail.com>
2019-05-05 02:17:51 -0400

    added play button

---
+++ Blockathon/Assets/Scripts/WinButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinButton : MonoBehaviour
+{
+    public string winScreen;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMovement>() != null)
+        {
+            SceneManager.LoadScene(winScreen);
+        }
+    }
+}
